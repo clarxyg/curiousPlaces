0fbf8f5f0ac168bb883b1b8cd06f4e3309bccd1d
clarxyg <clarxyg@gmail.com>
2021-09-22 19:44:39 -0300

    Created map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/dist/client/router'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+type Place = {
+  id: string
+  name: string
+  slug: string
+  location: {
+    latitude: number
+    longitude: number
+  }
+}
+export type MapProps = {
+  places?: Place[]
+}
+
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/clarxyz/cktupwrek1j2h17qfs8cs2zdr/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
+
+const Map = ({ places }: MapProps) => {
+  const router = useRouter()
+
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <CustomTileLayer />
+      {places?.map(({ id, name, slug, location }) => {
+        const { latitude, longitude } = location
+
+        return (
+          <Marker
+            key={`place-${id}`}
+            position={[latitude, longitude]}
+            title={name}
+            eventHandlers={{
+              click: () => {
+                router.push(`/place/${slug}`)
+              }
+            }}
+          />
+        )
+      })}
+    </MapContainer>
+  )
+}
+export default Map

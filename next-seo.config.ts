8b1785fdf4e59f63408d3fc57394ba440d42c2f0
clarxyg <clarxyg@gmail.com>
2021-09-22 19:40:36 -0300

    Add next-seo and configure

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://curiousplaces.com.br',
+    site_name: 'Curious Places'
+  },
+  twitter: {
+    handle: '@clarxyz',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}

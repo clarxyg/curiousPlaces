7c5bbb905fc618e4695d2372f2b1ae6282bcd5da
clarxyg <clarxyg@gmail.com>
2021-09-22 19:48:09 -0300

    Created places template

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,78 @@
+import { NextSeo } from 'next-seo'
+import Image from 'next/image'
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+      text: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <NextSeo
+        title={`${place.name} - Curious Places`}
+        description={
+          place.description?.text ||
+          'A simple project to show in a map curious places and them informations with photos.'
+        }
+        canonical="https://curiousplaces.com.br"
+        openGraph={{
+          url: 'https://curiousplaces.com.br',
+          title: `${place.name} - Curious Places`,
+          description:
+            place.description?.text ||
+            'A simple project to show in a map curious places and them informations with photos.',
+          images: [
+            {
+              url: place.gallery[0].url,
+              width: place.gallery[0].width,
+              height: place.gallery[0].height,
+              alt: `${place.name}`
+            }
+          ]
+        }}
+      />
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description.html }}
+          />
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}
